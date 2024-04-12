e22be0868871d104b390f5b04002c8097571a0df
Edu Machado <eduardomachadoh1@gmail.com>
2024-04-12 09:57:38 -0300

    Cenário 2

---
+++ Exercicio02/src/br/edu/up/App.java
@@ -0,0 +1,23 @@
+package br.edu.up;
+
+import br.edu.up.metodos.Ponto;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        Ponto ponto1 = new Ponto();
+
+        Ponto ponto2 = new Ponto(2, 5);
+
+        System.out.printf("Distância ponto 1 ao 2: %.1f\n", ponto2.calcularDistancia(ponto1.getX(), ponto1.getY()));
+        
+        ponto2.setX(7);
+        ponto2.setY(2);
+        System.out.printf("Distância ponto 1 ao 2: %.1f", ponto2.calcularDistancia(ponto1.getX(), ponto1.getY()));
+
+        ponto1.setX(10);
+        ponto1.setY(3);
+
+
+    }
+}
---
+++ Exercicio02/src/br/edu/up/metodos/Ponto.java
@@ -0,0 +1,42 @@
+package br.edu.up.metodos;
+
+public class Ponto {
+    private double x;
+    private double y;
+    
+    public Ponto(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+    
+    public Ponto() {
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double calcularDistancia(double x1, double y1) {
+        return Math.sqrt(Math.pow(x1 - this.x, 2) + Math.pow(y1 - this.y, 2));
+    }
+
+    
+
+    
+
+
+
+
+}
